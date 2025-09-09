9872cf14fa86d39417f7060032f3cc63bb8815f0
GreenIce99 <6508roshan@gmail.com>
2025-09-09 18:21:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-const CACHE_NAME = 'galaxy-runner-cache-v1';
+const CACHE_NAME = 'galaxy-runner-v2';
 const urlsToCache = [
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response=>response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
 });
