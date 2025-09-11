b8d99514196d01e872f446a189d771cdedc4e449
GreenIce99 <6508roshan@gmail.com>
2025-09-11 17:46:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = 'galaxy-runner-v2';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('galaxy-runner').then(cache=>{
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/game.js',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
 });
